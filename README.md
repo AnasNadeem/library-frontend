10568066aa7d7199a103e9817326a51acb477e81
Anas Nadeem <57933598+AnasNadeem@users.noreply.github.com>
2021-10-24 16:17:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Library Mangement System Frontend
+It's a **React.js** frontend application.
+
+I will be using Bootstrap for the designing.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      Library Management System.
+    </main>
+  );
+}
+
+export default App;
